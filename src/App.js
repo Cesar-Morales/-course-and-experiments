d164bdfc25a60092b914a3590fdf7e5ba62a2cb2
Cesar-Morales <moraleescesar@gmail.com>
2021-01-07 21:11:21 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Helloworld(){
+
+//-------------------------------------------------------------------
+//------------------Component Helloworld-----------------------------
+//-------------------------------------------------------------------
+function Helloworld(props){
+  //console.log(props)
   return(
-    <div id="hello">Hi! Im component named hello word</div>
+    <div id="hello">{props.rank} Hi {props.myname}! Im "COMPONATOR"</div>
   );
 }
+//-------------------------------------------------------------------------
+//-----------------------Same component------------------------------------
+//-------------------------------------------------------------------------
+//const Helloworld = () => <div id="hello">Hi! Im "COMPONATOR"</div>-------
+//-------------------------------------------------------------------------
+//--------------------------or---------------------------------------------
+//-------------------------------------------------------------------------
+//---class Hellowold extends React.Component{------------------------------
+//------render()-----------------------------------------------------------
+//--------return <div id="hello">Hi! Im Hi! Im component "COMPONATOR"</div>
+//------}------------------------------------------------------------------
+//---}---------------------------------------------------------------------
+
 
 function App() {
   return (
@@ -15,7 +33,12 @@ function App() {
         <p>
           Hello Pascu!
         </p>
-        <p>This is component: <Helloworld/></p>
+        <div>
+          This is component: 
+          <Helloworld myname="Chizzo" rank="(First)"/> 
+          <Helloworld myname="Tete"/> 
+          <Helloworld myname="Tanque" rank="3."/>
+        </div>
       </header>
     </div>
   );
