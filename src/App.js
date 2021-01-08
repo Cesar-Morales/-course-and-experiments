30412c9a6fa79decd1687d4d8a14269b5fee5569
Cesar-Morales <moraleescesar@gmail.com>
2021-01-07 21:51:45 -0300

    react state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react' // for class Helloworld
 
-
+/*
 //-------------------------------------------------------------------
 //------------------Component Helloworld-----------------------------
 //-------------------------------------------------------------------
-function Helloworld(props){
+ function Helloworld(props){
   //console.log(props)
   return(
     <div id="hello">{props.rank} Hi {props.myname}! Im "COMPONATOR"</div>
   );
-}
+} 
 //-------------------------------------------------------------------------
 //-----------------------Same component------------------------------------
 //-------------------------------------------------------------------------
@@ -18,11 +19,44 @@ function Helloworld(props){
 //-------------------------------------------------------------------------
 //--------------------------or---------------------------------------------
 //-------------------------------------------------------------------------
-//---class Hellowold extends React.Component{------------------------------
-//------render()-----------------------------------------------------------
-//--------return <div id="hello">Hi! Im Hi! Im component "COMPONATOR"</div>
-//------}------------------------------------------------------------------
-//---}---------------------------------------------------------------------
+*/
+class Helloworld extends React.Component{
+
+  state = {
+    show: false
+  }
+  toggleShow = () =>  {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if(this.state.show){
+      return (
+        <div id="hello">
+          {this.props.rank} Hi {this.props.myname}! Im "COMPONATOR"
+          <button onClick={this.toggleShow
+            /*------------3ro^^^
+            ()=>this.setState({show: false})  2do
+            alert('Component state: '+ this.state.show) 1ro*/}>
+            
+            Toggle State
+          </button>
+        </div>
+      )
+    }else{
+      return(
+        <div id="hello"> 
+          There are not element. Please toggle state
+          <button onClick={this.toggleShow
+            /*()=> this.setState({show: true}) 
+            alert('Component state: '+ this.state.show) */}>
+            Toggle State
+          </button>
+        </div>
+      )
+    }
+  }
+}
 
 
 function App() {
