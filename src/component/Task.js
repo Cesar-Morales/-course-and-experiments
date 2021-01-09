16633880abb16577527b078335bc18746a5c32aa
Cesar-Morales <moraleescesar@gmail.com>
2021-01-09 15:05:45 -0300

    btn

--- src/component/Task.js
+++ src/component/Task.js
@@ -1,18 +1,22 @@
 import {Component} from 'react';
 import './Task.css'
 
-class Task extends Component{
+class Task extends Component {
     
     state = {
         done: this.props.task.done
       }
-       
 
-    toggleState = () => {
+    btnDeactivate = () => {
         //console.log(this.state.done)
-        this.setState({done: !this.state.done}) ///FALTA CONECTAR STATE CON STYLE
+        this.setState({done: false}) ///FALTA CONECTAR STATE CON STYLE
     }
-    
+
+    btnActivate = () => {
+        //console.log(this.state.done)
+        this.setState({done: true}) ///FALTA CONECTAR STATE CON STYLE
+    }
+
     StyleCompleted(){
         //with func cant add conditional
         return{
@@ -22,41 +26,53 @@ class Task extends Component{
         }
     }
 
-
     render(){
-
         const {task} = this.props;
         //const redColor = {background: 'red'} # use style={redColor}
-        
+   
         return  <p style={this.StyleCompleted()}/*functionJS*/ /*style= {dark} (jsObject)*/ /*className= 'red' (cssFile) ---or--- style={{background: 'red'}} (inlineStyle) */>
                     ID: {task.id} - 
                     TITLE: {task.title} - \
                     DESCRIPTION: {task.description} 
                     <input type='checkbox'/>
-                    <button style={btnDelete} /* jsObject*/ onClick={this.toggleState}>
+                    <button style={btnDelete} /* jsObject*/ onClick={this.btnDeactivate}>
                         X
+                    </button>
+                    <button style={btnAccept} onClick= {this.btnActivate}>
+                        V
                     </button>
                 </p>
     }
-
 }
 
-// Javascript Object
-const btnDelete = {
-    fontSize: '18px',
-    background: 'red',
-    color: 'white',
-    border: '1px solid black',
-    padding: '10px 15px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    //float: 'right'
-}
+    // Javascript Object
+    const btnDelete = {
+        fontSize: '18px',
+        background: 'red',
+        color: 'white',
+        border: '1px solid black',
+        padding: '10px 15px',
+        borderRadius: '50%',
+        cursor: 'pointer',
+        //float: 'right'
+    }
 
-/* Javascript Object
- const dark = {
-    background: 'black',
-    color: 'white'
-}
- */
+    const btnAccept = {
+        fontSize: '18px',
+        background: 'green',
+        color: 'white',
+        border: '1px solid black',
+        padding: '10px 15px',
+        borderRadius: '50%',
+        cursor: 'pointer',
+        //float: 'right'
+    }
+
+    /* Javascript Object
+     const dark = {
+        background: 'black',
+        color: 'white'
+    }
+     */
+
 export default Task
