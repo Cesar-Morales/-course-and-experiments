1d81e932b1c0232339669d8a9d92eec913fe723e
Cesar-Morales <moraleescesar@gmail.com>
2021-01-12 10:32:40 -0300

    normalButton and touchableOpacityButton

--- App.js
+++ App.js
@@ -1,24 +1,48 @@
 import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, Button, Alert,
+TouchableOpacity, 
+ScrollView} from "react-native";
 import beagle from './assets/beagle.jpg'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world!!</Text>
-      <Image
-        source={{uri:"https://picsum.photos/300/300"}}
-        style={styles.images}>
-      </Image>
-      <Image style={styles.images}
-        source={beagle}>
-      </Image>
-    </View>
+    <ScrollView style={styles.scroll}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello world!!</Text>
+        <Image
+          source={{uri:"https://picsum.photos/300/300"}}
+          style={styles.images}>
+        </Image>
+        <Image style={styles.images}
+          source={beagle}>
+        </Image>
+        {/* normal button with console.log */}
+        <Button
+          title= 'console.log'
+          color= 'black'
+          onPress={ () =>  {console.log('button pressed')} }/>
+        {/* normal button with alert */}
+        <Button
+          title= 'alert'
+          color= 'gray'
+          onPress={ () =>  {Alert.alert('button pressed')} }/>
+        {/* customizable botton with alert*/}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={ () =>  {Alert.alert('button pressed')} }>
+          <Text>ToucheableOpacity</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scroll:{
+    backgroundColor: '#595959'
+  },
   container: { 
+    margin:30,
     flex: 1,
     justifyContent: "center", 
     alignItems: "center",
@@ -30,7 +54,13 @@ const styles = StyleSheet.create({
     height: 300,
     width: 300,
     borderRadius:150
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 7,
+    margin: 10
   }
+
 });
 
 export default App;
