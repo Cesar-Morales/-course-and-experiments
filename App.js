d9a86e70b8629538d49e1f4d9578bf8f86f024ce
Cesar-Morales <moraleescesar@gmail.com>
2021-01-12 12:43:43 -0300

    sharing

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { Text, View, StyleSheet, Image, Button, Alert,
 TouchableOpacity, ScrollView, ToastAndroid} from "react-native";
 import beagle from './assets/beagle.jpg'
 import * as ImagePicker from 'expo-image-picker'
+import * as Sharing from 'expo-sharing'
+
 
 const App = () => {
 
@@ -18,6 +20,7 @@ const App = () => {
     }
 
     const pickerResult = await ImagePicker.launchImageLibraryAsync()
+
     //console.log(pickerResult)
     if (pickerResult.cancelled === true){
       ToastAndroid.show('You didnt choose any image!',  ToastAndroid.LONG);
@@ -26,35 +29,68 @@ const App = () => {
 
     setSelectedImage({localUri: pickerResult.uri})
 
+  };
+
+  const loadAImage = () => {
+    ToastAndroid.show('Load a image',  ToastAndroid.SHORT);
+    return null
+  }
+
+  const openShareDialog = async() => {
+    //console.log("hello")
+    if (!(await Sharing.isAvailableAsync())){
+      alert("Sharing, is not available on your plataform");
+      return
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri)
   }
 
   return (
     <ScrollView style={styles.scroll}>
       <View style={styles.container}>
         <Text style={styles.text}>Pick a image</Text>
-        <Image
-          source={{uri: selectedImage !== null ? selectedImage.localUri : "https://picsum.photos/300/300"}}
-          style={styles.images}>
-        </Image>
+        
+        
+        <TouchableOpacity
+          onPress={ openImagePickerAsync }>
+          <Image
+            source={{
+              uri: 
+                selectedImage !== null 
+                ? selectedImage.localUri 
+                : "https://picsum.photos/300/300"
+              }}
+            style={styles.images}>
+          </Image>
+        </TouchableOpacity>
+        
+        
+        
         {/* customizable botton with alert*/}
         <TouchableOpacity
-          style={styles.button}
-          onPress={ openImagePickerAsync }>
-          <Text>Search image</Text>
+          style={ selectedImage ? styles.buttonEnabled : styles.buttonDisabled}
+          onPress={ selectedImage ? openShareDialog : loadAImage}>
+          <Text>{ selectedImage ? 'Share this image' : 'Load a image' }</Text>
         </TouchableOpacity>
         <Image style={styles.images}
           source={beagle}>
         </Image>
+        
+        
         {/* normal button with console.log */}
         <Button
           title= 'console.log'
           color= 'black'
           onPress={ () =>  {console.log('button pressed')} }/>
+        
+        
         {/* normal button with alert */}
         <Button
           title= 'alert'
           color= 'gray'
           onPress={ () =>  {Alert.alert('button pressed')} }/>
+      
       </View>
     </ScrollView>
   );
@@ -78,8 +114,13 @@ const styles = StyleSheet.create({
     width: 300,
     borderRadius:150
   },
-  button: {
+  buttonEnabled: {
     backgroundColor: 'green',
+    padding: 7,
+    margin: 10
+  },
+  buttonDisabled: {
+    backgroundColor: 'gray',
     padding: 7,
     margin: 10
   }
