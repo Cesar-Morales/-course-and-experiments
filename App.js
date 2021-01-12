7b7358ea107ec5565122b1b52c7020997b91d898
Cesar-Morales <moraleescesar@gmail.com>
2021-01-12 11:24:54 -0300

    Image Picker

--- App.js
+++ App.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import { Text, View, StyleSheet, Image, Button, Alert,
-TouchableOpacity, 
-ScrollView} from "react-native";
+TouchableOpacity, ScrollView, ToastAndroid} from "react-native";
 import beagle from './assets/beagle.jpg'
+import * as ImagePicker from 'expo-image-picker'
 
 const App = () => {
+
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    console.log('ToucheableOpacity')
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (permissionResult.granted === false){
+      alert('Permission to access camera is required!')
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync()
+    //console.log(pickerResult)
+    if (pickerResult.cancelled === true){
+      ToastAndroid.show('You didnt choose any image!',  ToastAndroid.LONG);
+      return null;
+    }
+
+    setSelectedImage({localUri: pickerResult.uri})
+
+  }
+
   return (
     <ScrollView style={styles.scroll}>
       <View style={styles.container}>
-        <Text style={styles.text}>Hello world!!</Text>
+        <Text style={styles.text}>Pick a image</Text>
         <Image
-          source={{uri:"https://picsum.photos/300/300"}}
+          source={{uri: selectedImage !== null ? selectedImage.localUri : "https://picsum.photos/300/300"}}
           style={styles.images}>
         </Image>
+        {/* customizable botton with alert*/}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={ openImagePickerAsync }>
+          <Text>Search image</Text>
+        </TouchableOpacity>
         <Image style={styles.images}
           source={beagle}>
         </Image>
@@ -26,12 +55,6 @@ const App = () => {
           title= 'alert'
           color= 'gray'
           onPress={ () =>  {Alert.alert('button pressed')} }/>
-        {/* customizable botton with alert*/}
-        <TouchableOpacity
-          style={styles.button}
-          onPress={ () =>  {Alert.alert('button pressed')} }>
-          <Text>ToucheableOpacity</Text>
-        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -56,7 +79,7 @@ const styles = StyleSheet.create({
     borderRadius:150
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: 'green',
     padding: 7,
     margin: 10
   }
