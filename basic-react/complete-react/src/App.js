e940980dc34abc2297f9a08c7cea309560ce7e96
Cesar-Morales <moraleescesar@gmail.com>
2021-03-01 19:35:08 -0300

    router

--- basic-react/complete-react/src/App.js
+++ basic-react/complete-react/src/App.js
@@ -1,139 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
-import pf from 'petfinder-client';
+import { Router } from "@reach/router";
+import Results from "./Results"
+import Details from "./Details"
 
-//import Songs from "./Songs";
-import Pet from "./Pet";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-})
-
-
-//Class component
-//class App extends React.Component {
-  //handleTitleClick() {
-  //  alert("you clicked the title");
-  //}
-  
-  //render() {
-    //return React.createElement("div", {}, [
-    //  React.createElement("h1", { onClick: this.handleTitleClick }, "Playlist"),
-    //  React.createElement("hr"),
-    //  React.createElement(Songs, {
-    //    name: "Barbazul",
-    //    album: "Glup",
-    //    interpreter: "Patricio Rey",
-    //    genero: "Rock",
-    //  }),
-    //  React.createElement(Songs, {
-    //    name: "Cantatas de Puentes Amarillos",
-    //    album: "Artaud",
-    //    interpreter: "Luis Alberto",
-    //    genero: "Rock",
-    //  }),
-    //]);
-class App extends React.Component {
-
-    constructor(props){
-      super(props);
-
-      this.state = {
-        pets: []
-      }
-
-    }
-
-  componentDidMount(){
-      petfinder.pet.find({output: 'full', location: "Seattle,WA"})
-        .then( data =>{
-          let pets;
-
-          if(data.petfinder.pets && data.petfinder.pets.pet){
-            if(Array.isArray(data.petfinder.pets.pet)) {
-              pets = data.petfinder.pets.pet;
-            } else{
-              pets = [data.petfinder.pets.pet];
-            }
-          } else {
-            pets = [];
-          }
-
-          this.setState({
-            pets
-          })
-        })
-    }
-
-    render() {
+class App extends React.Component{
+  render() {
     return (
-      <div>
-        <h1 /*onClick={this.handleTitleClick}*/>Adopt Me!</h1>
-        <hr/>
-        <div className="cards">
-          {this.state.pets.map(pet => {
-            let breed;
-
-            if(Array.isArray(pet.breeds.breed)){
-              breed = [pet.breeds.breed.join(" ,")];
-            }else{
-              breed = pet.breeds.breed;
-            }
-            
-            return(
-              <Pet 
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            )
-          })}
-        </div>
-        
-        {/* <Songs
-          name="Barbazul"
-          album="Glup"
-          interpreter="Patricio Rey"
-          genero="Rock"
-        />
-        <Songs
-          name="Cantatas de Puentes Amarillos"
-          album="Artaud"
-          interpreter="Luis Alberto"
-          genero="Rock"
-        ></Songs> */}
-        {/* DEBUG STATE 
-        <pre>
-          <code>{JSON.stringify(this.state, null, 4)}</code>
-        </pre>*/}
-      </div>
-    );
-  }
+    <div>
+      <h1>ADOPT ME</h1>
+      <Router>
+        <Results path="/" />
+        <Details path="/details/:id" />
+      </Router>
+    </div>
+  )}
 }
-
-/*
- * function component
-const App = () => {
-    return React.createElement('div', {}, [
-        React.createElement('h1', {}, 'Playlist'),
-        React.createElement('hr'),
-        React.createElement(Songs, {
-            name:"Barbazul",
-            album:"Glup",
-            interpreter:"Patricio Rey",
-            genero:"Rock"
-        }),
-        React.createElement(Songs, {
-            name:"Cantatas de Puentes Amarillos",
-            album:"Artaud",
-            interpreter:"Luis Alberto",
-            genero:"Rock"
-        })
-    ])
-}*/
 
 render(React.createElement(App), document.getElementById("root"));
