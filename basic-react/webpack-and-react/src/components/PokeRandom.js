8f7c7a28f41277f957059cf8bcca4b0c0768343d
Cesar-Morales <moraleescesar@gmail.com>
2021-03-11 19:11:16 -0300

    only 1080p

--- basic-react/webpack-and-react/src/components/PokeRandom.js
+++ basic-react/webpack-and-react/src/components/PokeRandom.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import pokedeck from '../static/pokedeck.png';
+import pokeBackground from '../static/background-card.jpeg';
+import pockemonBall from '../static/pockemonBall.png';
 class PokeRandom extends Component {
 
     state = {
        pokemon: {
-         name: 'NONE'
-       },
-       id: this.randomNum()
+          name: 'NONE',
+          id: this.randomNum(),
+        },
+        photo: pockemonBall,
+        background: pokeBackground
     }
 
     search() {
         fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.id}`)
         .then(res => res.json())
         .then((pokemon) => {
+          this.setState({ photo: pokemon.sprites.front_default }),
           this.setState({ pokemon })
         })
         .catch(console.log)
@@ -20,7 +25,7 @@ class PokeRandom extends Component {
 
     randomNum () {
       const min = Math.ceil(1);
-      const max = Math.floor(101);
+      const max = Math.floor(151);
       return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
@@ -32,12 +37,12 @@ class PokeRandom extends Component {
 
     render(){
       return (
-        <div>
-          <center>
-            <img src={pokedeck} height="500"/>
-            <h1>{this.state.pokemon.name}</h1>
-            <button onClick={this.getPokemon}>Random</button>
-          </center>
+        <div className="container">
+          <img src={pokedeck} height="500"/>
+          <img className="top-left" src={this.state.background}/>
+          <img className="top-left-img" src={this.state.photo}/>
+          <h1 className="top-right">{this.state.pokemon.name}</h1>
+          <p className="bottom-left" onClick={this.getPokemon}>Random</p>
         </div>
       )}
 };
